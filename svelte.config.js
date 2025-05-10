8437c4a18eb1c5d6253c3e81d534c9077bb1e41d
HipHopHuman <hiphophuman@hiphophuman.local>
2025-05-10 16:09:09 +0200

    more gh-pages tweaking

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,14 @@ import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-static';
 
 const config = {
-	kit: { adapter: adapter() },
+	kit: {
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : 'sharpeville-rhinos'
+		}
+	},
 	preprocess: [mdsvex()],
 	extensions: ['.svelte', '.svx']
 };
